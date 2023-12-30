68af634e7f6717c4399c8cec4f34515f3e1e3800
NimishKashyap <kashyap.nimish8@gmail.com>
2023-12-30 12:47:46 +0530

    encoding msg

--- app/main.go
+++ app/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"strings"
 
 	// Uncomment this block to pass the first stage
 	"net"
@@ -37,7 +38,8 @@ type DNSHeader struct {
 }
 
 type Message struct {
-	Header DNSHeader
+	Header   DNSHeader
+	Question Question
 }
 
 func newDNSHeader() *DNSHeader {
@@ -63,6 +65,91 @@ func (h *DNSHeader) toBytes() []byte {
 	binary.BigEndian.PutUint16(buf[10:12], h.ARCOUNT)
 
 	return buf
+}
+
+type (
+	CLASS uint16
+	TYPE  uint16
+)
+
+const (
+	TYPE_A     TYPE = 1
+	TYPE_NS    TYPE = 2
+	TYPE_MD    TYPE = 3
+	TYPE_MF    TYPE = 4
+	TYPE_CNAME TYPE = 5
+	TYPE_SOA   TYPE = 6
+	TYPE_MB    TYPE = 7
+	TYPE_MG    TYPE = 8
+	TYPE_MR    TYPE = 9
+	TYPE_NULL  TYPE = 10
+	TYPE_WKS   TYPE = 11
+	TYPE_PTR   TYPE = 12
+	TYPE_HINFO TYPE = 13
+	TYPE_MINFO TYPE = 14
+	TYPE_MX    TYPE = 15
+	TYPE_TXT   TYPE = 16
+)
+
+const (
+	CLASS_IN CLASS = 1
+	CLASS_CS CLASS = 2
+	CLASS_CH CLASS = 3
+	CLASS_HS CLASS = 4
+)
+
+type Question struct {
+	QNAME  []byte
+	QTYPE  TYPE
+	QCLASS CLASS
+}
+
+func NewQuestion() *Question {
+	return &Question{
+		QNAME:  []byte{},
+		QTYPE:  TYPE_A,
+		QCLASS: CLASS_IN,
+	}
+}
+
+func (q *Question) toBytes() []byte {
+	buf := make([]byte, 4+len(q.QNAME))
+	copy(buf[0:], q.QNAME)
+	binary.BigEndian.PutUint16(buf[len(q.QNAME):len(q.QNAME)+2], uint16(q.QTYPE))
+	binary.BigEndian.PutUint16(buf[len(q.QNAME)+2:len(q.QNAME)+4], uint16(q.QCLASS))
+
+	return buf
+}
+
+func (m *Message) encodeDomains(domains []string) {
+	for _, domain := range domains {
+		labels := strings.Split(domain, ".")
+		for _, label := range labels {
+			m.Question.QNAME = append(m.Question.QNAME, byte(len(label)))
+			m.Question.QNAME = append(m.Question.QNAME, label...)
+		}
+	}
+	m.Question.QNAME = append(m.Question.QNAME, '\x00')
+	m.Header.QDCOUNT = uint16(len(domains))
+}
+
+func (m *Message) toBytes() []byte {
+	buf := make([]byte, 0)
+	buf = append(buf, m.Header.toBytes()...)
+	buf = append(buf, m.Question.toBytes()...)
+
+	return buf
+}
+
+func buildSampleResponse() []byte {
+	header := newDNSHeader()
+	question := NewQuestion()
+	message := Message{
+		Header:   *header,
+		Question: *question,
+	}
+	message.encodeDomains([]string{"codecrafters.io"})
+	return message.toBytes()
 }
 
 func main() {
@@ -98,8 +185,10 @@ func main() {
 
 		// Create an empty response
 		// response := []byte{}
-		header := newDNSHeader()
-		response := header.toBytes()
+		// header := newDNSHeader()
+		// response := header.toBytes()
+		//
+		response := buildSampleResponse()
 
 		_, err = udpConn.WriteToUDP(response, source)
 		if err != nil {
